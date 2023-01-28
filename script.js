13128b00bcce3b007ea1f4a870f665755c1044a8
Sameer Md <sameermd@Sameers-MacBook-Pro.local>
2023-01-29 02:42:23 +0530

    minor changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const arrow = document.querySelector(".arrow");
+const social_media = document.querySelector(".socials_share")
+
+
+arrow.addEventListener("click", () => {
+    this.classList.toggle("active");
+   social_media.classList.toggle("active");
+});
